ba9b7c86a12e916184a030e3c8dca03d1ee634a5
Roccofranchini <rocco.franchini1@gmail.com>
2021-07-22 15:08:23 +0200

    fx1

--- js/script.js
+++ js/script.js
@@ -1,35 +1,38 @@
 const card = {
 	name: "Anaba Shaman",
-    manaCost: ["3", "R"],
-    combinedManaCost = 4,
-    cardType: 'Creature',
-    subTybe: 'Minotaur Shaman',
-    expansion: {
-        reprintId: 9,
-        name: 'Ninth Edition',
-        rarity: 'Common',
-        collectionNr: "177/350 ",
-    },
+	manaCost: ["3", "R"],
+	combinedManaCost: 4,
+	cardType: "Creature",
+	subTybe: "Minotaur Shaman",
+	expansion: {
+		reprintId: 9,
+		name: "Ninth Edition",
+		rarity: "Common",
+		collectionNr: "177/350 ",
+	},
 
-    flavorText: {
-        quote: 'Just try taking this bull by the horns.',
-        author: '',
-    },
-    abilities: [{
-        launchCost: ['R', 'T'],
-        description: 'Anaba Shaman deals 1 damage to target creature or player.',
-    }],
-    illustration: {
-        id: 1,
-        author: 'Simon Bisley',
-        immage: './img/pic.jpg',
-
-    },
-    constitution: 2,
-    strength: 2,
-    borderColor: "#000",
-
-
-
-
+	flavorText: {
+		quote: "Just try taking this bull by the horns.",
+		author: "",
+	},
+	abilities: [
+		{
+			launchCost: ["R", "T"],
+			description: "Anaba Shaman deals 1 damage to target creature or player.",
+		},
+	],
+	illustration: {
+		id: 1,
+		author: "Simon Bisley",
+		immage: "./img/pic.jpg",
+	},
+	constitution: 2,
+	strength: 2,
+	borderColor: "#000",
+	background: {
+		color: "red",
+		source: "./img/background.jpg",
+	},
 };
+
+console.log(card);
